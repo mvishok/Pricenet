cb4d612e53a93acff35bbabe65f277c4999f08a8
M Vishok <vishokmanikantan@gmail.com>
2024-05-26 19:10:26 +0530

    chore: Update Flipkart scraper to include trust information in the scraped data

--- scraper/flipkart.js
+++ scraper/flipkart.js
@@ -2,7 +2,7 @@ import {load} from 'cheerio';
 import bent from 'bent';
 
 const flipkart = async (q) => {
-    const get = bent('https://www.flipkart.com', 'GET', 'string', 200);
+    const get = bent('https://www.flipkart.com/search?q=', 'GET', 'string', 200);
     const response = await get(
         q
     );
@@ -22,6 +22,10 @@ const flipkart = async (q) => {
         }
         const desc = $(element).find('.NqpwHC').text();
         const img = $(element).find('img').attr('src');
+        let trust = false;
+        if ($(element).find('.dVXNbG').length > 0){
+            trust = true;
+        }
 
         data.push({ 
             "title": title,
@@ -30,9 +34,11 @@ const flipkart = async (q) => {
             "price": price,
             "freeDelivery": freeDelivery,
             "desc": desc,
-            "img": img
+            "img": img,
+            "trust": trust
         });
     });
+
     $('.cPHDOP').each((index, element) => {
         const title = $(element).find('.KzDlHZ').text();
         if (!title || title == '') return;
@@ -47,6 +53,10 @@ const flipkart = async (q) => {
         }
         const desc = $(element).find('._6NESgJ').find('li').map((index, element) => $(element).text().trim()).get().join(", ")
         const img = $(element).find('img').attr('src');
+        let trust = false;
+        if ($(element).find('._0CSTHy').length > 0){
+            trust = true;
+        }
 
         data.push({ 
             "title": title,
@@ -55,7 +65,8 @@ const flipkart = async (q) => {
             "price": price,
             "freeDelivery": freeDelivery,
             "desc": desc,
-            "img": img
+            "img": img,
+            "trust": trust
         });
     });
     return data;
