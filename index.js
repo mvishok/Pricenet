8e31c7662781bd62444844be2311bb505b2f01c7
M Vishok <vishokmanikantan@gmail.com>
2024-05-27 18:25:37 +0530

    chore: Update server port to 8080

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ fastify.get('/search', async (request, reply) => {
 });
 
 try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: 8080 })
 } catch (err) {
     fastify.log.error(err)
     process.exit(1)
